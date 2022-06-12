a7262b6081fa641b3ac59d3247d2efc851d65be8
ih060899 <ih060899@gmail.com>
2022-06-13 00:27:09 +0600

    add left and center in header component

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+    images: {
+        domains: [
+            'upload.wikimedia.org',
+            'avatars.dicebear.com',
+            'images.pexels.com',
+            'platform-lookaside.fbsbx.com',
+            'static.xx.fbcdn.net',
+        ],
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
